48412290f5f68a72a306c4bfdb5a79280671b0f1
Lucas Messias <42678807+soumessias@users.noreply.github.com>
2023-05-07 23:17:19 -0300

    Delete lib directory

--- lib/utils/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const authOrHome = '/';
-  static const loginPage = '/login';
-  static const objectivesPage = '/objectives';
-  static const signupPage = '/signup';
-  static const passRecoveryPage = '/passrecovery';
-  static const profilePage = '/profile';
-}
